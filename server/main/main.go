f06225e08aa01f15db702ba94fb1c8cb6ab25f8b
Debsnil24 <debsnilsamudra@gmail.com>
2025-03-06 03:03:44 +0530

    Implemented the Main Function **Summary** - Created the Skeleton for the To-Do list UI - Implemented the code to Initialize the localhost server **To-Do** - Complete the UI - Update the .env file with relevant data - Implement the code for DB Connection - Implement the routes and the models - Implement the handler and control function for DB interaction - Test and fix bugs as they occur

--- server/main/main.go
+++ server/main/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Debsnil24/React-Go-ToDo.git/router"
+)
+
+func main() {
+	r := router.Router()
+	fmt.Println("Starting the server on port 9000")
+
+	err := http.ListenAndServe(":9000", r)
+	if err != nil {
+		log.Fatalf("Error: Unable to start Webserver at port 9000 \n %v", err)
+	}
+}
