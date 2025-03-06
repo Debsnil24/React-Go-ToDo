21d04707ee79849596efb78314b844a494221e12
Debsnil24 <debsnilsamudra@gmail.com>
2025-03-06 07:32:03 +0530

    Implemented the router and models **Summary** - Created routes to controller function using the mux router pkg - Created a basic model of the data to be stored in the DB - Imported the necessary packages **To-Do** - Complete the UI - Update the .env file with relevant data - Implement the code for DB Connection - Implement the handler and control function for DB interaction - Test and fix bugs as they occur

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,8 @@
 module github.com/Debsnil24/React-Go-ToDo.git
 
 go 1.24.0
+
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	go.mongodb.org/mongo-driver v1.17.3 // indirect
+)
--- server/router/routes.go
+++ server/router/routes.go
@@ -1,1 +1,16 @@
 package router
+
+import (
+	"github.com/Debsnil24/React-Go-ToDo.git/middleware"
+	"github.com/gorilla/mux"
+)
+
+func Router() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/api/task",middleware.GetAllTask).Methods("GET")
+	router.HandleFunc("/api/task/create",middleware.CreateTask).Methods("POST")
+	router.HandleFunc("/api/task/complete",middleware.CompleteTask).Methods("PUT")
+	router.HandleFunc("/api/task/edit",middleware.EditTask).Methods("PUT")
+	router.HandleFunc("/api/task/delete",middleware.DeleteTask).Methods("DELETE")
+	return router
+}
