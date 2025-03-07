8dcedd65abdc1bd73299102e3fe30673bb1c7794
Debsnil24 <debsnilsamudra@gmail.com>
2025-03-07 20:14:54 +0530

    Implemented the UI **Summary** - Implemented the Basic UI - Implemented Basic backend Connection - Modified routes to make it easier to understand **To-Do** - Write the CSS - Test and Debug - Write Documentation

--- server/router/routes.go
+++ server/router/routes.go
@@ -9,10 +9,10 @@ func Router() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/task",middleware.GetAllTask).Methods("GET","OPTIONS")
 	router.HandleFunc("/api/task/create",middleware.CreateTask).Methods("POST","OPTIONS")
-	router.HandleFunc("/api/task/{id}",middleware.CompleteTask).Methods("PUT","OPTIONS")
-	router.HandleFunc("/api/task/{id}",middleware.UndoTask).Methods("PUT","OPTIONS")
-	router.HandleFunc("/api/task/{id}",middleware.EditTask).Methods("PUT","OPTIONS")
-	router.HandleFunc("/api/task/{id}",middleware.DeleteTask).Methods("DELETE","OPTIONS")
+	router.HandleFunc("/api/Comptask/{id}",middleware.CompleteTask).Methods("PUT","OPTIONS")
+	router.HandleFunc("/api/Undotask/{id}",middleware.UndoTask).Methods("PUT","OPTIONS")
+	router.HandleFunc("/api/Edittask/{id}",middleware.EditTask).Methods("PUT","OPTIONS")
+	router.HandleFunc("/api/Deltask/{id}",middleware.DeleteTask).Methods("DELETE","OPTIONS")
 	router.HandleFunc("/api/task/delete",middleware.DeleteAll).Methods("DELETE","OPTIONS")
 	return router
 }
